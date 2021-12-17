f51881493a64e766b9b01376d334595d92819227
EricNur <74959047+EricNur@users.noreply.github.com>
2021-12-17 17:13:01 +0700

    Update Tubes.cpp

--- Tubes.cpp
+++ Tubes.cpp
@@ -1,1 +1,46 @@
 #include "Tubes.h"
+
+void create_list(mll &list_penyanyi){
+
+}
+
+void newElm_penyanyi(string penyanyi, adr_penyanyi &P){
+
+}
+
+void newElm_lagu(string judul, string jmlPendengar, float rating, adr_lagu &L){
+
+}
+
+void insertNew_Penyanyi(mll &list_penyanyi, adr_penyanyi P){
+
+}
+
+void insertNew_Lagu(mll &list_penyanyi, adr_penyanyi P, adr_lagu L){
+
+}
+
+void delete_penyanyi(mll &list_penyanyi, adr_penyanyi &P){
+  
+}
+
+void delete_lagu(mll &list_penyanyi, adr_penyanyi P, adr_lagu &L){
+
+}
+
+void show_penyanyi(mll list_penyanyi){
+
+}
+
+void show_lagu(mll list_penyanyi, adr_penyanyi P){
+
+}
+
+void find_penyanyi(mll list_penyanyi){
+
+}
+
+void find_lagu(mll list_penyanyi, adr_penyanyi P){
+
+}
+
